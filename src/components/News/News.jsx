7fb8152b3f3d1aeccc369a1ed90c5ee48ef4414b
Anupam Nair <86453853+anupamnair93@users.noreply.github.com>
2021-09-25 13:53:38 +0530

    [cryptoverse]: added cryptodetails with exchanges and charts

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi';
+import { useGetCryptoCoinsQuery } from '../../services/cryptoApi';
 require('dotenv').config();
 
 const { Text, Title } = Typography;
@@ -11,10 +12,12 @@ const { Option } = Select;
 const demoImage = process.env.REACT_APP_NEWS_DEMO_IMAGE;
 
 const News = ({ simplified }) => {
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
     const { data: cryptoNews } = useGetCryptoNewsQuery({
-        newsCategory: 'CryptoCurrency',
+        newsCategory,
         count: simplified ? 6 : 12,
     });
+    const { data } = useGetCryptoCoinsQuery(100);
 
     if (!cryptoNews?.value) return 'Loading...';
 
@@ -25,9 +28,19 @@ const News = ({ simplified }) => {
                     <Select
                         showSearch
                         className='select-news'
-                        placeholder='Select a Crypto Currency'
+                        placeholder='Select a Crypto'
                         optionFilterProp='children'
-                        onChange={(value) => console.log(value)}></Select>
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) =>
+                            option.children
+                                .toLowerCase()
+                                .indexOf(input.toLowerCase()) >= 0
+                        }>
+                        <Option value='Cryptocurrency'>Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => (
+                            <Option value={coin.name}>{coin.name}</Option>
+                        ))}
+                    </Select>
                 </Col>
             )}
             {cryptoNews?.value?.map((news, i) => (
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -17,7 +17,22 @@ export const cryptoApi = createApi({
         getCryptoCoins: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
         }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) =>
+                createRequest(`coin/${coinId}/history/${timeperiod}`),
+        }),
     }),
 });
 
-export const { useGetCryptoCoinsQuery } = cryptoApi;
+export const {
+    useGetCryptoCoinsQuery,
+    useGetCryptoDetailsQuery,
+    useGetExchangesQuery,
+    useGetCryptoHistoryQuery,
+} = cryptoApi;
